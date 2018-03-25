2cb0a41d61b120ffa80588b3bb0093e6f2ffb957
Alex Drenea <alexdrenea@hotmail.com>
2018-03-24 21:23:06 -0400

    - initial commit

---
+++ CosmosDB.Repl/Utilities/ConsoleLogger.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CosmosDB.Repl.Utilities
+{
+   public class ConsoleLogger : ILogger
+    {
+        private readonly LogLevel _level;
+        public ConsoleLogger(LogLevel level = LogLevel.All)
+        {
+            _level = level;
+        }
+
+        public void Error(string message, Exception ex)
+        {
+            Console.WriteLine($"ERROR: {message}{Environment.NewLine}{ex.Message}{Environment.NewLine}-----");
+        }
+
+        public void Info(string message)
+        {
+            if (_level <= LogLevel.Info)
+                Console.WriteLine($"INFO: {message}");
+        }
+
+        public void Verbose(string message)
+        {
+            if (_level <= LogLevel.All)
+                Console.WriteLine($"VERBOSE: {message}");
+        }
+
+        public void Warning(string message)
+        {
+            if (_level <= LogLevel.Warning)
+                Console.WriteLine($"Warning: {message}");
+        }
+    }
+}
---
+++ CosmosDB.Repl/Utilities/ILogger.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace CosmosDB.Repl.Utilities
+{
+    public enum LogLevel
+    {
+        All,
+        Info,
+        Warning,
+        Error
+    }
+    public interface ILogger
+    {
+        void Verbose(string message);
+        void Info(string message);
+        void Warning(string message);
+        void Error(string message, Exception ex);
+    }
+}
